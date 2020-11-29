ee9a5177182378dc981d8122933b087014a43888
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2020-11-29 11:27:00 -0500

    BST update

--- src/BST/iBSTAVL.java
+++ src/BST/iBSTAVL.java
@@ -5,11 +5,7 @@ import java.util.ArrayList;
 public interface iBSTAVL<E,K extends Comparable<? super K>> {
 	
 	
-	int depth(Node<E, K> current);
-	
-	int checkAVLBalance();
-	
-	
-	ArrayList<E> toArrayList();
+
+
 	
 }
